be07eb25b0cc6356d1e429f0f85f0979dacc27ce
xurui <xurui185@gmail.com>
2020-03-31 15:58:41 +0800

    add q67-68-69-69II offer & update 38

---
+++ Qsixty6.cpp
@@ -0,0 +1,22 @@
+//
+// Created by xurui on 2020/3/31.
+//
+
+class Solution {
+public:
+    vector<int> constructArr(vector<int>& a) {
+        int n = a.size();
+        vector<int> ret(n, 1);
+        int left = 1;
+        for (int i = 0; i < n; i ++) {
+            ret[i] = left;
+            left  *= a[i];
+        }
+        int right = 1;
+        for (int i = n-1; i >= 0; i --) {
+            ret[i] *= right;
+            right *= a[i];
+        }
+        return ret;
+    }
+};
--- Qthirty8.cpp
+++ Qthirty8.cpp
@@ -2,6 +2,7 @@
 // Created by xurui on 2020/3/15.
 //
 #include <vector>
+#include <string>
 using  namespace std;
 /* 解题思路：
  * 1.使用dfs算法
@@ -13,7 +14,7 @@ public:
         dfs(s,0);
         return res;
     }
-    void dfs(string& s, int pos){
+    void dfs(string s, int pos){
         if(pos >= s.size()){
             res.push_back(s);
             return;
@@ -21,16 +22,15 @@ public:
         for(int i = pos; i < s.size(); i++){
             if(judge(s,pos,i)) continue;
             swap(s[pos],s[i]);
-            dfs(s,pos+1);
+            dfs(s,pos);
             swap(s[pos],s[i]);
         }
+
     }
-    //如果有重复就跳过
-    bool judge(string& s, int start, int end){
+    bool judge(string s, int start, int end){
         for(int i = start; i < end; i++){
-            if(s[i] == s[end]){
+            if(s[i] == s[end])
                 return true;
-            }
         }
         return false;
     }
